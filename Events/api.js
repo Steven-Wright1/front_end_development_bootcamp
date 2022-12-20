9fcb11b1ea86def5f954813706f0b8b3ee997852
Steven-Wright1 <steven.wright593@gmail.com>
2022-12-20 18:48:59 +0000

    second API query

--- Events/api.js
+++ Events/api.js
@@ -1,22 +1,41 @@
 
 const search_button = document.querySelector('#btn');
 const search = document.querySelector('#query');
-
+let performer_id; 
 //console.log(search_button)
 
 function event(e){
 
-
-let clientid = `MzEwOTk2MTJ8MTY3MTQ3MzczMi43OTQ0ODUz`; 
+let clientid = `MzEwOTk2MTJ8MTY3MTQ3MzQ0My4wNzgyMjU`; 
 let artist = search.value;
 let apirequest = `https://api.seatgeek.com/2/performers?q=${artist}&client_id=${clientid}`;
 
-console.log(apirequest);
-
 fetch(apirequest).then(response=>{
-    console.log(response.json());
+    //console.log(response.json())
+    return response.json();
+})
+.then((data) =>{
+    let performer_id = data.performers[0].id;
+
+    let apirequest2 = `https://api.seatgeek.com/2/events?performers.id=${performer_id}&client_id=${clientid}`;
+    console.log(apirequest2)
+    fetch(apirequest2).then(response=>{
+        //console.log(response.json())
+        console.log( response.json());
+    })
 });
+
+
+
+
+
+
+
+
+
+
 
 }
 
 search_button.addEventListener('click', event);
+ 
